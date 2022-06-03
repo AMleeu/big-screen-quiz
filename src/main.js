08d9d0db82725a8a9102aedc4d27d559373af703
AM leeu <albert.m.leeu@gmail.com>
2022-06-02 23:37:26 -0800

    installed bootstrap & bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.config.productionTip = false;
+// Make BootstrapVue available throughout project
+Vue.use(BootstrapVue);
+// Make BootstrapVue BootstrapVue icon components available throughout project
+Vue.use(IconsPlugin);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
