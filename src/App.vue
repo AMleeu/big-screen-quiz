2f40bb74c7a08e39b691fda94ac9ba0452286e08
AM leeu <albert.m.leeu@gmail.com>
2022-06-03 09:59:07 -0800

    added custom Header component with basic styling & static data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+      <Header /> 
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue"
 
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
@@ -19,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
