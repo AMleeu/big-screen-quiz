e60eb2f40b631349db9c0a93a87f48e8f7f05af8
AM leeu <albert.m.leeu@gmail.com>
2022-06-04 12:05:47 -0800

    added list of answers to QuestionBox & styled hover state of each answer

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,14 +1,19 @@
 <template>
   <b-jumbotron>
     <template #lead>
-       <span v-html="currentQuestion.question "></span>
+      <span v-html="currentQuestion.question"></span>
     </template>
 
     <hr class="my-4" />
 
-    <p>
-      list of answers
-    </p>
+    <b-list-group class="mb-4">
+      <b-list-group-item
+        v-for="(answer, index) in answersCompilation"
+        :key="index"
+      >
+        {{ answer }}
+      </b-list-group-item>
+    </b-list-group>
 
     <b-button variant="primary" class="mr-3" href="#">SUBMIT</b-button>
     <b-button variant="success" href="#" @click="next">NEXT</b-button>
@@ -19,7 +24,22 @@
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
-  }
-}
+    next: Function,
+  },
+  computed: {
+    answersCompilation() {
+      // compile correct answer & incorrect answers in one array
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+
+      return answers;
+    },
+  },
+};
 </script>
+
+<style scoped>
+  .list-group-item:hover{
+    background-color: #e9ecef;
+  }
+</style>
