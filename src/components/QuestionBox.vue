877f1c80973b8b5d5cab455c8614cfccc9fff7ad
AM leeu <albert.m.leeu@gmail.com>
2022-06-04 12:43:19 -0800

    installed lodash & used it to shuffle list of answers in QuestionBox

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,10 +8,10 @@
 
     <b-list-group class="mb-4">
       <b-list-group-item
-        v-for="(answer, index) in answersCompilation"
+        v-for="(answer, index) in answers"
         :key="index"
+        v-html="answer"
       >
-        {{ answer }}
       </b-list-group-item>
     </b-list-group>
 
@@ -21,25 +21,50 @@
 </template>
 
 <script>
+// it is common practice to import lodash as _
+import _ from "lodash";
+
 export default {
   props: {
     currentQuestion: Object,
     next: Function,
   },
-  computed: {
-    answersCompilation() {
-      // compile correct answer & incorrect answers in one array
+  data() {
+    return {
+      answers: [],
+    };
+  },
+  methods: {
+    shuffleAnswer() {
+      // append correct to array of incorrect answers
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
 
-      return answers;
+      // shuffle the answers such that the correct answer is not always last
+      this.answers = _.shuffle(answers);
+    },
+  },
+  watch: {
+    currentQuestion: {
+      /* whenever a new value is passed to currentQuestion prop is passed
+        run the shuffleAnswer method
+      */
+
+      /* make is so that this starts right away as opposed to wait for the user
+        to navigate to next question
+        that is - treat the initially passed currentQuestion value as a new as well
+      */
+      immediate: true,
+      handler() {
+        this.shuffleAnswer();
+      },
     },
   },
 };
 </script>
 
 <style scoped>
-  .list-group-item:hover{
-    background-color: #e9ecef;
-  }
+.list-group-item:hover {
+  background-color: #e9ecef;
+}
 </style>
